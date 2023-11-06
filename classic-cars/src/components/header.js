de665f80ae4167196b960570202472b6016a73d4
Tomasz Dębosz <98646427+Dosztosz@users.noreply.github.com>
2023-11-06 20:42:44 +0100

    update

---
+++ classic-cars/src/components/header.js
@@ -0,0 +1,25 @@
+import logo from "../logo.svg";
+
+function Header() {
+  const data = [
+    { name: "Main Page" },
+    { name: "Rent" },
+    { name: "About" },
+    { name: "Contact" },
+  ];
+  const listItems = data.map((d) => (
+    <li key={d.name}>
+      <a href={d.name}>{d.name}</a>
+    </li>
+  ));
+
+  return (
+    <div class="header">
+      <img src={logo} alt="logo" width="100px" />
+      <nav>
+        <ul class="flex-list">{listItems}</ul>
+      </nav>
+    </div>
+  );
+}
+export default Header;
